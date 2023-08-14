f1b75a14b8b6791fc4a6df888e781c2d8b1b65ac
shodiqimamp <shodiqimamp@gmail.com>
2023-08-15 06:38:36 +0700

    remove unnecessary escapes

--- client/src/components/VideoDetail.js
+++ client/src/components/VideoDetail.js
@@ -71,7 +71,7 @@ function VideoDetail() {
   };
   const getYoutubeId = (url) => {
     const regExp =
-      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
 
     if (match && match[2].length === 11) {
